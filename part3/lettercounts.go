9d01815a1198dcb0ba80dde29d5251a7e08f260b
Matthew Whisenhunt <mattwhisenhunt@gmail.com>
2016-01-22 01:48:41 -0800

    Add Json() as an explanation of aforementioned inflection point.

--- part3/lettercounts.go
+++ part3/lettercounts.go
@@ -2,11 +2,21 @@ package part3
 
 import (
 	"strings"
+	"log"
 	"unicode/utf8"
 	"sort"
+	"strconv"
 )
 
 /*
+
+I ended having to make a trade off: Provide a data structure that could be indexed like
+a built in map or keep track of rune lengths at insert and alocate memory for output at O(1)
+
+I added Json() as an example of spending O(n) to calculate the size of memory to allocate
+for the output string. Even it had another trade off loop through once and append/allocate as
+I went along or O(n) twice and just do one allocation. I don't know the right answer.
+
 */
 
 type Lettercounts struct {
@@ -36,6 +46,38 @@ func (l Lettercounts) String() string {
 	l.Foreach(func(v uint, k rune) {
 		pos += utf8.EncodeRune(str[pos:], k)
 	})
+	return string(str)
+}
+
+func (l Lettercounts) Json() string {
+
+	length := 1
+	for k, v := range l.letters {
+		length += 4 + utf8.RuneLen(k) + numDigits(v)
+	}
+	str := make([]byte, length)
+	pos := 1
+	str[0] = '{'
+	str[length-1] = '}'
+	comma := false
+	l.Foreach(func(v uint, k rune) {
+		if comma {
+			str[pos] = ',' ; pos += 1
+		} else {
+			comma = true
+		}
+
+		str[pos] = '"' ; pos += 1
+		pos += utf8.EncodeRune(str[pos:], k)
+		str[pos] = '"' ; pos += 1
+		str[pos] = ':' ; pos += 1
+		digits := strconv.Itoa(int(v))
+		for i :=0 ; i < len(digits) ; i++ {
+			str[pos] = digits[i]
+			pos += 1
+		}
+	})
+	log.Println(str)
 	return string(str)
 }
 
@@ -70,4 +112,14 @@ func (a byCountAlpha) Less(i, j int) bool {
 		return a[i].k < a[j].k
 	}
 	return a[i].v > a[j].v // Greater than to sort DESC
+}
+
+func numDigits(x uint) int{
+	if x < 10 {return 1}
+	if x < 100 {return 2}
+	if x < 1000 {return 3}
+	if x < 10000 {return 4}
+	if x < 100000 {return 5}
+	if x < 1000000 {return 6}
+	return 21;
 }
